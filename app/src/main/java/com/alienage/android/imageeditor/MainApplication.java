c91611312352fec2e9df8da0d9057dbcf8265893
prashant patel <prashant.chovatiya@eliteinfoworld.com>
2018-03-13 11:25:40 +0530

    initial commit v1.0

--- app/src/main/java/com/alienage/android/imageeditor/MainApplication.java
+++ app/src/main/java/com/alienage/android/imageeditor/MainApplication.java
@@ -11,9 +11,21 @@ import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 
 public class MainApplication extends Application implements IAdobeAuthClientCredentials {
 
-    private static final String CREATIVE_SDK_CLIENT_ID      = "<-- CLIENT ID -->";
-    private static final String CREATIVE_SDK_CLIENT_SECRET  = "<-- CLIENT SECRET -->";
-    private static final String CREATIVE_SDK_REDIRECT_URI   = "<-- REDIRECT URI -->";
+    private static final String CREATIVE_SDK_CLIENT_ID      = "20035243f9a146c681d59786a35ad3c0";
+    private static final String CREATIVE_SDK_CLIENT_SECRET  = "150db997-9f46-4c4c-9257-492d68a60c0d";
+    private static final String CREATIVE_SDK_REDIRECT_URI   = "ams+1ec29e2038842e210604c832ffb0ef2a92ca441f://adobeid/20035243f9a146c681d59786a35ad3c0";
+
+    /*
+    *
+    *
+        private static final String CREATIVE_SDK_CLIENT_ID      = "<-- CLIENT ID -->";
+        private static final String CREATIVE_SDK_CLIENT_SECRET  = "<-- CLIENT SECRET -->";
+        private static final String CREATIVE_SDK_REDIRECT_URI   = "<-- REDIRECT URI -->";
+    *
+    *
+    */
+
+
     private static final String[] CREATIVE_SDK_SCOPES = {"email", "profile", "address"};
 
     @Override
